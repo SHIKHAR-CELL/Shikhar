98eb6ef31bd36554af6b948e98385b431f76af3f
SHIKHAR-CELL <bhardwajshikhar934@gmail.com>
2024-10-14 00:39:45 -0700

    changes

---
+++ day6/script.js
@@ -0,0 +1,35 @@
+// let a=()=>{
+//     console.log("arrow function");
+// };
+// a();
+// let b=()=>console.log("arrow function 2");
+// b();
+// a();
+// let c=(x,y)=>{
+//     return x+y;
+// }
+// let res=c('rahul',"ravi");
+// let res1=c(12,32);
+// console.log(res,res1);
+// function display(){
+//     // let p1=document.querySelector('p');
+//     let p1=document.querySelectorAll('p');
+//     p1[0].innerHTML="i am in parasf";               //query selector selects only 1st apearence of single element
+//     p1[0].style.backgroundColor="red";
+//     p1[1].innerHTML="i am in parasf2";              // queryselector all selects multiple elements
+//     p1[1].style.backgroundColor="green";
+    
+// }
+// let changeheader=()=>{
+//     const header=document.querySelector('h1');
+//     header.textContent='new heading';
+//     let a1=document.querySelector('a');
+//     a1.setAttribute("href",'https://aktu.ac.in');
+//     a1.innerHTML="AKTU WESBITE";
+// };
+// let addcontent=()=>{
+//     const newdiv=document.createElement('div');
+//     newdiv.textContent="this is a new div";
+//     const body=document.querySelector('body');
+//     body.appendChild(newdiv);
+// }
